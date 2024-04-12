540af4a67feed4012c710c802819a91d034135e6
loony2m <flew.bird@mail.ru>
2024-04-12 18:36:00 +0300

    123

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
-const squares = document.querySelectorAll(".square");
-const placeholders = document.querySelectorAll(".placeholder");
+const squares = document.querySelectorAll('.square');
+const placeholders = document.querySelectorAll('.placeholder');
+
+const links = document.querySelectorAll('#input-button');
+const inputs = document.querySelectorAll('#input');
+
+links.forEach((link, index) => {
+  link.addEventListener('click', () => {
+    inputs[index].value = '';
+  });
+});
 
 function addSquareEventListeners(square) {
-  square.addEventListener("dragstart", dragstart);
-  square.addEventListener("dragend", dragend);
+  square.addEventListener('dragstart', dragstart);
+  square.addEventListener('dragend', dragend);
 }
 
 function dragstart(event) {
-  event.target.classList.add("hold");
-  setTimeout(() => event.target.classList.add("hide"), 0);
+  event.target.classList.add('hold');
+  setTimeout(() => event.target.classList.add('hide'), 0);
 }
 
 function dragend(event) {
-  event.target.classList.remove("hold", "hide");
+  event.target.classList.remove('hold', 'hide');
   saveStateToLocalStorage();
 }
 
@@ -21,24 +30,24 @@ function dragover(event) {
 }
 
 function dragenter(event) {
-  event.target.classList.add("hovered");
+  event.target.classList.add('hovered');
 }
 
 function dragleave(event) {
-  event.target.classList.remove("hovered");
+  event.target.classList.remove('hovered');
 }
 
 function dragdrop(event) {
-  event.target.classList.remove("hovered");
+  event.target.classList.remove('hovered');
 
-  const draggedElement = document.querySelector(".square.hold");
+  const draggedElement = document.querySelector('.square.hold');
   if (draggedElement) {
     const clone = draggedElement.cloneNode(true);
-    clone.classList.remove("hold", "hide");
+    clone.classList.remove('hold', 'hide');
     event.target.appendChild(clone);
     addSquareEventListeners(clone);
 
-    clone.addEventListener("contextmenu", (e) => {
+    clone.addEventListener('contextmenu', (e) => {
       e.preventDefault();
       if (e.button === 2) {
         clone.remove();
@@ -48,27 +57,20 @@ function dragdrop(event) {
 }
 
 placeholders.forEach((placeholder) => {
-  placeholder.addEventListener("dragover", dragover);
-  placeholder.addEventListener("dragenter", dragenter);
-  placeholder.addEventListener("dragleave", dragleave);
-  placeholder.addEventListener("drop", dragdrop);
+  placeholder.addEventListener('dragover', dragover);
+  placeholder.addEventListener('dragenter', dragenter);
+  placeholder.addEventListener('dragleave', dragleave);
+  placeholder.addEventListener('drop', dragdrop);
 });
 
 squares.forEach((square) => {
-  square.addEventListener("dragstart", dragstart);
-  square.addEventListener("dragend", dragend);
+  square.addEventListener('dragstart', dragstart);
+  square.addEventListener('dragend', dragend);
 });
 
-function clearHabit(element) {
-  let inputElement = element.parentNode.querySelector(".habit-input");
-  if (inputElement) {
-    inputElement.value = "";
-  }
-}
-
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', function () {
   // Получаем все элементы input с классом "user-data"
-  let inputElements = document.querySelectorAll(".user-data");
+  let inputElements = document.querySelectorAll('.user-data');
 
   // Проходим по каждому элементу
   inputElements.forEach(function (inputElement) {
@@ -82,18 +84,12 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     // Получаем кнопку по её id, предположим, что у кнопки id="clear-button"
-    let clearButton = document.getElementById("clear-button");
+    let clearButton = document.getElementById('clear-button');
 
     // Добавляем обработчик события для события input
-    inputElement.addEventListener("input", function () {
+    inputElement.addEventListener('input', function () {
       // Обновляем значение в локальном хранилище при вводе
       localStorage.setItem(inputElement.id, inputElement.value);
-    });
-
-    // Добавляем обработчик события для клика на кнопку
-    clearButton.addEventListener("click", function clearHabit() {
-      // Очищаем значение поля ввода
-      inputElement.value = "";
     });
   });
 });
